9f758a49df6bc417d48121d5b951abaf56d1bebf
Quentin <quentin@affholder.fr>
2025-09-16 17:53:48 +0200

    feat: v2 script.js + v1 index.html

--- script/script.js
+++ script/script.js
@@ -2,16 +2,24 @@ const choix = ["Pierre", "Feuille", "Ciseau"];
 let scoreJoueur = 0;
 let scoreOrdinateur = 0;
 let scoreEgalite = 0;
+let nombreMatches = 0;
 
 function jouer(choixJoueur) {
     const choixOrdinateur = choix[Math.floor(Math.random()*choix.length)];
     if (choixJoueur==choixOrdinateur){
         scoreEgalite++;
+        nombreMatches++;
     }
-    else if ((choixJoueur=='Pierre') && (choixOrdinateur=='Ciseau') || (choixJoueur=='Ciseau') && (choixOrdinateur == 'Feuille') || (choixJoueur=='Feuille') || (choixOrdinateur == 'Pierre')) {
+    else if ((choixJoueur=='Pierre') && (choixOrdinateur=='Ciseau') || (choixJoueur=='Ciseau') && (choixOrdinateur == 'Feuille') || (choixJoueur=='Feuille') && (choixOrdinateur == 'Pierre')) {
         scoreJoueur++;
+        nombreMatches++;
     }
     else {
         scoreOrdinateur++;
+        nombreMatches++;
     }
+    document.getElementById("score-joueur").innerText = scoreJoueur;
+    document.getElementById("score-ordinateur").innerText = scoreOrdinateur;
+    document.getElementById("score-nul").innerText = scoreEgalite;
+    document.getElementById("nombre-matches").innerText = nombreMatches;
 }
